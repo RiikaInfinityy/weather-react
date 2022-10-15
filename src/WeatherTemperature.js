bc8035254b252aeb7a31788ccdd7e064395420de
Rika Lim <limmingxian@live.com>
2022-10-15 14:12:21 +0200

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -6,7 +6,8 @@ export default function WeatherTemperature(props) {
   return (
     <div className="WeatherTemperature">
       <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
+      <span className="unit">°C</span> <span className="unit">|</span>{" "}
+      <span className="unit">°F</span>
     </div>
   );
 }
