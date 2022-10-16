a29889ad7182e4d87e62abd0506a217aacfd4fce
Rika Lim <limmingxian@live.com>
2022-10-16 16:22:10 +0200

    shortened forecast day words

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -18,15 +18,7 @@ export default function WeatherForecastDay(props) {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
 
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fr", "Sat"];
 
     return days[day];
   }
